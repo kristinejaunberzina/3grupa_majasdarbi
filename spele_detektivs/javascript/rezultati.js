12d093bb12246498e2af08a4123d4345b17dc798
armandspucs <53978384+armandspucs@users.noreply.github.com>
2020-01-10 08:49:24 +0200

    a

---
+++ spele_detektivs/javascript/rezultati.js
@@ -0,0 +1,71 @@
+
+
+    //iegūstam spēlētāja vārdu
+    var vards = prompt("Ievadet savu vārdu", "Tālis");
+
+    //iegūstam punktus
+    punkti = window.location.hash;
+    punkti = punkti.substr(1, 2);
+
+    //http request objekti kas darīs komunikāciju ar repl.it
+    var xhr = new XMLHttpRequest();//rezultātu saņemšanai
+    var xhr2 = new XMLHttpRequest();//nosūtīšanai
+
+
+    //rezultātu ierakstīšana
+    xhr2.open('GET', 'https://rezultati-spelei.armandspucs.repl.co/detektivs?vards='+vards+'&punkti='+punkti, true);
+    xhr2.send();
+
+    
+    confirm("Jūsu rezultāts ir saglabāts!");
+
+
+    //rezultātu iegūšana zemāk
+    
+    function rezultatus_iegut()
+    {
+    xhr.open('GET', "https://rezultati-spelei.armandspucs.repl.co/detektivs", true);
+    xhr.send();
+    
+    //kad kaut kas iegūts no repl.it izsauc rezultatus_paradit(e) 
+    xhr.addEventListener("readystatechange", rezultatus_paradit, false);
+    }
+
+
+    rezultatus_iegut();
+    
+    
+    function rezultatus_paradit(e) { //e = info no repl.it server
+        if (xhr.readyState == 4 && xhr.status == 200) { //ja vispār kaut kas saņemts
+            
+            //rezultatus pārvērš JSON objektā
+            var rezultati = JSON.parse(xhr.responseText);
+
+            //visu liksim DIV
+            var rezultati_div = document.getElementById('rezultati');
+            rezultati_div.innerHTML = '<table>';
+
+            //loop rezultatus lai var parādīt lapā
+            for (i = 0; i < rezultati['rezultati'].length; i++) 
+            {
+                //liekam iekšā DIV
+                rezultati_div.innerHTML = rezultati_div.innerHTML + '<tr>';
+                rezultati_div.innerHTML = rezultati_div.innerHTML + '<td>';
+                rezultati_div.innerHTML = rezultati_div.innerHTML + rezultati['rezultati'][i]['vards'];
+                rezultati_div.innerHTML = rezultati_div.innerHTML + '</td>';
+                rezultati_div.innerHTML = rezultati_div.innerHTML + ' ';
+                rezultati_div.innerHTML = rezultati_div.innerHTML + '<td>';
+                rezultati_div.innerHTML = rezultati_div.innerHTML + rezultati['rezultati'][i]['punkti'];
+                rezultati_div.innerHTML = rezultati_div.innerHTML + '</td>';
+                rezultati_div.innerHTML = rezultati_div.innerHTML + '</tr>';
+                rezultati_div.innerHTML = rezultati_div.innerHTML + ' ';
+            }
+
+            rezultati_div.innerHTML = rezultati_div.innerHTML + '</table>';
+
+        }
+    }
+    //////////////////////////////////////////
+    //beidzas rezultātu iegūšana
+
+  
